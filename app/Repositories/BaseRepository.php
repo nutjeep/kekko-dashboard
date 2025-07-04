39d8e33f8f8092ecfbdf1a8e9ab7bb89634ea9f2
nutjeep <anajibmuhammad@gmail.com>
2025-07-04 17:08:41 +0700

    Add: files needed

---
+++ app/Repositories/BaseRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+
+abstract class BaseRepository
+{
+  protected $model;
+
+  public function __construct(Model $model)
+  {
+    $this->model = $model;
+  }
+
+  public function get()
+  {
+    return $this->model->get();
+  }
+
+  public function find($id)
+  {
+    return $this->model->find($id);
+  }
+
+  public function create(array $data)
+  {
+    return $this->model->create($data);
+  }
+
+  public function update($id, array $data)
+  {
+    return $this->model->find($id)->update($data);
+  }
+
+  public function delete($id)
+  {
+    return $this->model->find($id)->delete();
+  }
+}
